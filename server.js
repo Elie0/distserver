0e5d7bafe30043e5aa6d6a8b38c75da78ebdf4a5
Elie Aro <elie.alarou@cmeoffshore.com>
2023-09-28 17:26:27 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+console.log("TESTTT")
+const localIP = '192.168.1.5';
+const express = require('express');
+const path = require('path');
+const port = 4000;
+
+
+const app = express();
+
+// Serve static files from the "dist" directory
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch all other routes and return the "index.html" file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
